3d7382f847057152a71f6aaf206907098582f6ef
AlainDLC <jd_alain@hotmail.com>
2017-02-09 11:36:22 +0100

    klasser
    getter and setters public kunstoktor

--- src/main/java/entities/sub/CloudType.java
+++ src/main/java/entities/sub/CloudType.java
@@ -2,12 +2,43 @@ package entities.sub;
 
 import entities.CoreEntity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Michael Sjögren on 2017-02-09.
  */
+
+@Entity
+
 public class CloudType extends CoreEntity {
-    @Override
-    public long getID() {
-        return id;
+
+    public CloudType(String name, WeatherInfo weatherinfo){
+        super();
+        this.name = name;
+        this.weatherinfo=weatherinfo;
+
+
     }
+
+    private String name;
+    private WeatherInfo weatherinfo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WeatherInfo getWeatherinfo() {
+        return weatherinfo;
+    }
+
+    public void setWeatherinfo(WeatherInfo weatherinfo) {
+        this.weatherinfo = weatherinfo;
+    }
+
+
 }
--- src/main/java/entities/sub/WeatherStation.java
+++ src/main/java/entities/sub/WeatherStation.java
@@ -2,12 +2,26 @@ package entities.sub;
 
 import entities.CoreEntity;
 
+import javax.persistence.Entity;
+
 /**
  * Created by Michael Sjögren on 2017-02-09.
  */
+@Entity
 public class WeatherStation extends CoreEntity {
-    @Override
-    public long getID() {
-        return id;
+
+    public WeatherStation(String name) {
+        this.name = name;
     }
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
