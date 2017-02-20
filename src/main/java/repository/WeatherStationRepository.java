256ba740ab5cf696a33bc573513e7bedfaa02bdb
AlainDLC <jd_alain@hotmail.com>
2017-02-20 09:04:52 +0100

    Revert "Inserting eteaterSation by new data"
    This reverts commit e393df5d6dcaa67224f6d87d3ab7768aa068722b.

--- src/main/java/repository/WeatherStationRepository.java
+++ src/main/java/repository/WeatherStationRepository.java
@@ -1,17 +1,10 @@
 package repository;
 
-import entities.sub_entity.WeatherInfo;
 import entities.sub_entity.WeatherStation;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by Michael Sjögren on 2017-02-13.
  */
 public interface WeatherStationRepository extends CrudRepository<WeatherStation , Long> {
-    ArrayList<WeatherStation> findById(long id);
-    ArrayList<WeatherStation> findByWeatherStationId(long id);
-    
 }
